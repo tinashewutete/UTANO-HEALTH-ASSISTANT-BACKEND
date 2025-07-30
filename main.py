fb461366b459a335c67fb4462b1426dcc0f701a8
nashegraphix <nashegraphix@gmail.com>
2025-07-30 15:16:40 +0200

    Updated vercel url

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 # Enable CORS (allow all origins — change in production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://utano-health-assistant.vercel.app/", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
